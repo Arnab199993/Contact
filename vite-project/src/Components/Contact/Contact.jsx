c2f0623694ea5d26a67d2e2e1e0fd0032356a0e1
Arnab199993 <103571005+Arnab199993@users.noreply.github.com>
2023-04-23 17:59:00 +0530

    Woxro Project

---
+++ vite-project/src/Components/Contact/Contact.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Contact.css"
+const Contact = () => {
+    return (
+        <div>
+            <div className='Contact'>
+                <div style={{ display: "flex" }}>
+                    <div className='Contactdiv'>
+                        <span className='contactfirst'>We offer all kinds of IT services that</span><br></br>
+                        <span className='contactSecond'>Ensure you success</span>
+                    </div>
+                    <div className='Btn'>
+                        <button className='Button'>Contact Us</button>
+                        <button className='Button1'>Lets Talk</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Contact
---
+++ vite-project/src/Components/Products/Products.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import "./Products.css"
+const Products = () => {
+    return (
+        <div className='Main'>
+            <div className='Prodwithimage'>
+                <div style={{ height: "340px", width: "360px", marginTop: "50px", border: "1px solid #E7F0F7", marginLeft: "100px", backgroundColor: "#E7F0F7", borderRadius: "10px", cursor: "pointer" }}>
+                    <div className='ImageProduct'>
+                        <img src='https://woxro.com/public/assets/png/services/webdesigning.png' />
+                    </div>
+                    <div className='Bio'>
+                        <p className='Web'>Web Designing</p>
+                    </div>
+                    <div className='Text'>
+                        We create impressive interfaces. Usability +
+                        user experience are created based on a<br></br>
+                        study of preferences and user behavior,<br></br>
+                        using the latest web design trends, our <br></br>
+                        experience, and achievements.
+                    </div>
+                </div>
+                <div style={{ height: "340px", width: "360px", marginTop: "50px", border: "1px solid #F6F5F3", marginLeft: "20px", backgroundColor: "#F6F5F3", borderRadius: "10px", cursor: "pointer" }}>
+                    <div className='ImageProduct'>
+                        <img src='https://woxro.com/public/assets/png/services/webapplication.png' />
+                    </div>
+                    <div className='Bio'>
+                        <p className='Web'>Web Application</p>
+                    </div>
+                    <div className='Text'>
+                        We create impressive interfaces. Usability +
+                        user experience are created based on a<br></br>
+                        study of preferences and user behavior,<br></br>
+                        using the latest web design trends, our <br></br>
+                        experience, and achievements.
+                    </div>
+                </div>
+                <div style={{ height: "340px", width: "360px", marginTop: "50px", border: "1px solid #F6F5F3", marginLeft: "20px", backgroundColor: "#F6F5F3", borderRadius: "10px", cursor: "pointer" }}>
+                    <div className='ImageProduct'>
+                        <img src='https://woxro.com/public/assets/png/services/mobileappdevelopment.png' />
+                    </div>
+                    <div className='Bio'>
+                        <p className='Web'>Mobile App Developement</p>
+                    </div>
+                    <div className='Text'>
+                        We create impressive interfaces. Usability +
+                        user experience are created based on a<br></br>
+                        study of preferences and user behavior,<br></br>
+                        using the latest web design trends, our <br></br>
+                        experience, and achievements.
+                    </div>
+                </div>
+            </div>
+
+            <div className='SecondLines'>
+                <div style={{ height: "340px", width: "360px", marginTop: "20px", border: "1px solid #FcF0E4", marginLeft: "250px", backgroundColor: "#FcF0E4", borderRadius: "10px", cursor: "pointer" }}>
+                    <div className='ImageProduct'>
+                        <img src='https://woxro.com/public/assets/png/services/ecommercedevelopment.png' />
+                    </div>
+                    <div className='Bio'>
+                        <p className='Web'>Ecommerce Developement</p>
+                    </div>
+                    <div className='Text'>
+                        We create impressive interfaces. Usability +
+                        user experience are created based on a<br></br>
+                        study of preferences and user behavior,<br></br>
+                        using the latest web design trends, our <br></br>
+                        experience, and achievements.
+                    </div>
+                </div>
+                <div style={{ height: "340px", width: "360px", marginTop: "20px", border: "1px solid #E7F0F7", marginLeft: "20px", backgroundColor: "#E7F0F7", borderRadius: "10px", cursor: "pointer" }}>
+                    <div className='ImageProduct'>
+                        <img src='	https://woxro.com/public/assets/png/services/digitalmarketing.png' />
+                    </div>
+                    <div className='Bio'>
+                        <p className='Web'>Digital Marketing</p>
+                    </div>
+                    <div className='Text'>
+                        We create impressive interfaces. Usability +
+                        user experience are created based on a<br></br>
+                        study of preferences and user behavior,<br></br>
+                        using the latest web design trends, our <br></br>
+                        experience, and achievements.
+                    </div>
+                </div>
+                <div style={{ height: "340px", width: "360px", marginTop: "20px", border: "1px solid #EFFBFB", marginLeft: "20px", backgroundColor: "#EFFBFB", borderRadius: "10px", cursor: "pointer" }}>
+                    <div className='ImageProduct'>
+                        <img src='https://woxro.com/public/assets/png/services/graphic.png' />
+                    </div>
+                    <div className='Bio'>
+                        <p className='Web'>Graphic Designing</p>
+                    </div>
+                    <div className='Text'>
+                        We create impressive interfaces. Usability +
+                        user experience are created based on a<br></br>
+                        study of preferences and user behavior,<br></br>
+                        using the latest web design trends, our <br></br>
+                        experience, and achievements.
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Products
